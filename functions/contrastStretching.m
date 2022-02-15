abfb05dec6f317b5423ca0363555798e7ab0012a
Matthew Kevin Amadeus <13518035@std.stei.itb.ac.id>
2022-02-15 17:58:37 +0700

    feat: cringe contrast adjustment

--- functions/contrastStretching.m
+++ functions/contrastStretching.m
@@ -1,28 +1,35 @@
 function output = contrastStretching(input, a, b)
     image_size = size(input);
-    for chan = image_size(3);
-        % get input for current channel
-        chan_input = input(:, :, chan);
+    disp(image_size);
 
-        % get min and max contrast for the channel input based on specified
-        % threshold
-        rmin = min(chan_input(chan_input > a));
-        rmax = max(chan_input(chan_input < b));
-           
-        % instantiate output
-        output = zeros(image_size);
-        
-        % c o n t r a s t s t r e c h i n g
-        for i= 1:image_size(1)
-            for j=1:image_size(2)
-                if (chan_image(i, j) <= rmin)
-                    output(i, j, chan) = 0;
-                elseif (chan_image(i, j) >= rmax)
-                    output(i, j, chan) = 255;
-                else
-                    output(i, j, chan) = floor(255 * (chan_image(i, j) - rmin) / (rmax - rmin));
-                end
+    % get min and max contrast for the channel input based on specified
+    % threshold
+    min_value = min(min(input));
+    max_value = max(max(input));
+    
+    disp(min_value);
+    disp(max_value);
+
+    rmin = (max_value-min_value)*a + min_value;
+    rmax = (max_value-min_value)*b + min_value;
+
+    disp(rmin);
+    disp(rmax);
+       
+    % instantiate output
+    output = zeros(image_size);
+    
+    % c o n t r a s t s t r e c h i n g
+    for i= 1:image_size(1)
+        for j=1:image_size(2)
+            if (input(i, j) <= rmin)
+                output(i, j) = 0;
+            elseif (input(i, j) >= rmax)
+                output(i, j) = 255;
+            else
+                output(i, j) = floor(double(255) * double(input(i, j) - rmin) / double(rmax - rmin));
+                % disp(output(i,j))
             end
         end
-    end 
-end
+    end
+end 
--- testScript.m
+++
@@ -1,5 +0,0 @@
-A = 1:4;
-a = grayHistogram("images/flask.png", "png");
-
-bar(a);
-imhist(rgb2gray(imread("./images/flask.png")));
